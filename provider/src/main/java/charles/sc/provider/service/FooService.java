a92bd71d4068e4e4cafacde4ca382835cd638ec4
Charles <charles.v.he@gmail.com>
2016-12-15 15:05:17 +0800

    add config

--- provider/src/main/java/charles/sc/provider/service/FooService.java
+++ provider/src/main/java/charles/sc/provider/service/FooService.java
@@ -2,6 +2,8 @@ package charles.sc.provider.service;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,15 +12,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * Created by Charles on 2016/8/16.
  */
+@RefreshScope
 @Service
 @RequestMapping("/foo")
 @ResponseBody
 public class FooService {
     private Log log = LogFactory.getLog(FooService.class);
 
+    @Value("${foo:foo}")
+    private String foo;
+
     @RequestMapping(method = RequestMethod.GET)
     public String bar() {
         log.info("FooService.bar");
-        return "bar " + System.currentTimeMillis();
+        return "bar " + foo +System.currentTimeMillis();
     }
 }
