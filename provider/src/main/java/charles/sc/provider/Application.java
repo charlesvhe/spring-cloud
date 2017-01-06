8686e93ecf7e7bd69a8791277f602eef5fbf25fe
charleshe <charles.v.he@gmail.com>
2017-01-06 16:02:09 +0800

    tomcat支持http2

--- provider/src/main/java/charles/sc/provider/Application.java
+++ provider/src/main/java/charles/sc/provider/Application.java
@@ -1,7 +1,13 @@
 package charles.sc.provider;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http2.Http2Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +31,27 @@ public class Application {
                         .allowedOrigins("*")
                         .allowedMethods("POST", "DELETE", "PUT", "GET", "OPTIONS");
             }
+        };
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer tomcatCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                    ((TomcatEmbeddedServletContainerFactory) container)
+                            .addConnectorCustomizers(new TomcatConnectorCustomizer() {
+                                @Override
+                                public void customize(Connector connector) {
+                                    connector.addUpgradeProtocol(new Http2Protocol());
+                                }
+
+                            });
+                }
+            }
+
         };
     }
 
